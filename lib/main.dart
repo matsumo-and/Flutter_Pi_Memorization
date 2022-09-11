aa4887ba3d3c7d6f2524ce2b8af8b27276bbec0d
matsumo-and <yh134.toisanda@gmail.com>
2022-09-11 15:19:51 +0900

    set canvas color

--- lib/main.dart
+++ lib/main.dart
@@ -9,20 +9,25 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
+  static const Color canvasColor = Color.fromRGBO(236, 239, 241, 1);
+  static const AppBarTheme appBarTheme = AppBarTheme(
+      elevation: 1,
+      backgroundColor: Colors.white,
+      foregroundColor: Colors.black,
+      titleTextStyle: TextStyle(
+        color: Colors.black,
+        fontSize: 16,
+      ));
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Pi_Memorization',
       theme: ThemeData(
-          primarySwatch: Colors.blue,
-          appBarTheme: const AppBarTheme(
-              elevation: 1,
-              backgroundColor: Colors.white,
-              foregroundColor: Colors.black,
-              titleTextStyle: TextStyle(
-                color: Colors.black,
-                fontSize: 16,
-              ))),
+        primarySwatch: Colors.blue,
+        appBarTheme: appBarTheme,
+        canvasColor: canvasColor,
+      ),
       home: const BottomTabBar(),
       debugShowCheckedModeBanner: false,
     );
