7608eeadfececad3f157c4d4a468955a708a18a6
matsumo-and <yh134.toisanda@gmail.com>
2022-09-12 01:00:29 +0900

    import google fonts

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import 'view/bottom_tab_bar.dart';
 
@@ -33,11 +34,13 @@ class MyApp extends StatelessWidget {
           primarySwatch: Colors.blue,
           appBarTheme: appBarTheme,
           canvasColor: canvasColor,
-          textTheme: const TextTheme(
-            headline1: TextStyle(
-              color: Colors.black,
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
+          textTheme: TextTheme(
+            headline1: GoogleFonts.notoSansJavanese(
+              textStyle: const TextStyle(
+                color: Color.fromRGBO(33, 33, 33, 1),
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
             ),
           )),
       home: const BottomTabBar(),
--- lib/view/multiplication.dart
+++ lib/view/multiplication.dart
@@ -6,8 +6,8 @@ class Multiplication extends StatelessWidget {
 
   static const EdgeInsets cardMargin =
       EdgeInsets.symmetric(vertical: 7, horizontal: 12);
-  static final BeveledRectangleBorder border =
-      BeveledRectangleBorder(borderRadius: BorderRadius.circular(7));
+  static final RoundedRectangleBorder border =
+      RoundedRectangleBorder(borderRadius: BorderRadius.circular(7));
 
   @override
   Widget build(BuildContext context) {
@@ -23,10 +23,12 @@ class Multiplication extends StatelessWidget {
         child: Column(children: [
           Padding(
             padding: cardMargin,
-            child: Text(
-              '実戦コース',
-              style: Theme.of(context).textTheme.headline1,
-              textAlign: TextAlign.left,
+            child: Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                '実戦コース',
+                style: Theme.of(context).textTheme.headline1,
+              ),
             ),
           ),
           Card(
@@ -38,7 +40,38 @@ class Multiplication extends StatelessWidget {
                 width: MediaQuery.of(context).size.width,
                 child: Row(children: [
                   Medal.none.icon,
+                ]),
+              )),
+          const SizedBox(height: 30),
+          Padding(
+            padding: cardMargin,
+            child: Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                '演習コース',
+                style: Theme.of(context).textTheme.headline1,
+              ),
+            ),
+          ),
+          Card(
+              margin: cardMargin,
+              color: Colors.white,
+              shape: border,
+              child: SizedBox(
+                height: 80,
+                width: MediaQuery.of(context).size.width,
+                child: Row(children: [
                   Medal.beginner.icon,
+                ]),
+              )),
+          Card(
+              margin: cardMargin,
+              color: Colors.white,
+              shape: border,
+              child: SizedBox(
+                height: 80,
+                width: MediaQuery.of(context).size.width,
+                child: Row(children: [
                   Medal.professional.icon,
                 ]),
               )),
