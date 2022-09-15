39cfc363ed69167c27fff955998efcf595494c6e
matsumo-and <yh134.toisanda@gmail.com>
2022-09-16 02:32:19 +0900

    add comment

--- lib/controller/multiplication_store.dart
+++ lib/controller/multiplication_store.dart
@@ -2,6 +2,7 @@ import 'package:flutter_pi_memorization/model/multiplication/multiplication_stor
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:hive/hive.dart';
 
+///globalに宣言し、bottom_tab_bar.dartで初期化される
 final multiplicationProvider =
     StateNotifierProvider<MultiplicationStore, List<Multiplication>>((ref) {
   return MultiplicationStore([]);
@@ -12,6 +13,7 @@ class MultiplicationStore extends StateNotifier<List<Multiplication>> {
 
   late Box box;
 
+  ///ローカルストレージから掛け算の成績を取得し、Riverpodによる状態管理をする
   Future<void> initialize() async {
     box = await Hive.openBox('box');
 
@@ -22,6 +24,7 @@ class MultiplicationStore extends StateNotifier<List<Multiplication>> {
     }
   }
 
+  ///unique id をキーとしてどの掛け算の問題か区別する。
   Multiplication find(int id) {
     return state.firstWhere((element) => element.id == id,
         orElse: () => const Multiplication());
