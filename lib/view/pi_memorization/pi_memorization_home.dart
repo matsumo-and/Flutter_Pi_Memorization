583ff315a38d65518ad85eb13450398b4e125400
matsumo-and <yh134.toisanda@gmail.com>
2022-09-28 01:24:35 +0900

    [feat]円周率を表示用に10桁ごと改行する

--- lib/view/pi_memorization/pi_memorization_home.dart
+++ lib/view/pi_memorization/pi_memorization_home.dart
@@ -9,17 +9,39 @@ class PiMemorizationHome extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final pi50 = Pi.piAll.substring(0, 50);
+    RegExp reg = RegExp(r'(\d{10})(?=(\d{10})+)');
+    String mathFunc(Match match) => '${match[1]}\n';
+    List<String> result = pi50.replaceAllMapped(reg, mathFunc).split('\n');
+
     return Scaffold(
       appBar: const HomeAppBar(title: Text("円周率")),
       body: SingleChildScrollView(
-        child: Column(
-          children: [
-            TappableCard(
-              height: 400,
-              child: Text(Pi.piAll.substring(0, 50)),
-              onTap: null,
-            )
-          ],
+        child: Container(
+          color: Colors.white,
+          margin: const EdgeInsets.symmetric(vertical: 15),
+          width: MediaQuery.of(context).size.width,
+          child: Column(
+            children: result
+                .map((e) => Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: e
+                          .split('')
+                          .map((e) => Container(
+                                margin:
+                                    const EdgeInsets.symmetric(vertical: 10),
+                                width: MediaQuery.of(context).size.width / 10,
+                                height: MediaQuery.of(context).size.width / 10,
+                                child: Text(
+                                  e,
+                                  style: TextStyle(fontSize: 28),
+                                  textAlign: TextAlign.center,
+                                ),
+                              ))
+                          .toList(),
+                    ))
+                .toList(),
+          ),
         ),
       ),
     );
