eefd77785b60504c190fe0b1357023beeddd4232
matsumo-and <yh134.toisanda@gmail.com>
2022-09-29 01:24:22 +0900

    [feat]円周率ホームの上部Widgetを実装

--- lib/view/pi_memorization/pi_memorization_home.dart
+++ lib/view/pi_memorization/pi_memorization_home.dart
@@ -1,47 +1,69 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_pi_memorization/view/common_appbar.dart';
 import 'package:flutter_pi_memorization/view/multiplication/tappable_card.dart';
-
-import '../../model/pi_memorization/pi.dart';
+import 'package:flutter_pi_memorization/view/pi_memorization/pi_view.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class PiMemorizationHome extends StatelessWidget {
   const PiMemorizationHome({Key? key}) : super(key: key);
 
+  static const EdgeInsets _padding =
+      EdgeInsets.symmetric(vertical: 7, horizontal: 12);
+
   @override
   Widget build(BuildContext context) {
-    final pi50 = Pi.piAll.substring(0, 50);
-    RegExp reg = RegExp(r'(\d{10})(?=(\d{10})+)');
-    String mathFunc(Match match) => '${match[1]}\n';
-    List<String> result = pi50.replaceAllMapped(reg, mathFunc).split('\n');
-
     return Scaffold(
       appBar: const HomeAppBar(title: Text("円周率")),
       body: SingleChildScrollView(
-        child: Container(
-          color: Colors.white,
-          margin: const EdgeInsets.symmetric(vertical: 15),
-          width: MediaQuery.of(context).size.width,
-          child: Column(
-            children: result
-                .map((e) => Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: e
-                          .split('')
-                          .map((e) => Container(
-                                margin:
-                                    const EdgeInsets.symmetric(vertical: 10),
-                                width: MediaQuery.of(context).size.width / 10,
-                                height: MediaQuery.of(context).size.width / 10,
-                                child: Text(
-                                  e,
-                                  style: TextStyle(fontSize: 28),
-                                  textAlign: TextAlign.center,
-                                ),
-                              ))
-                          .toList(),
-                    ))
-                .toList(),
-          ),
+        child: Column(
+          children: [
+            Container(
+              color: Colors.white,
+              margin: const EdgeInsets.symmetric(vertical: 15),
+              width: MediaQuery.of(context).size.width,
+              child: Column(children: [
+                //円周率の各桁一覧表
+                const PiView(digitsId: 1),
+
+                Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(width: 0.5, color: Colors.grey),
+                    borderRadius: const BorderRadius.all(Radius.circular(7)),
+                  ),
+                  margin:
+                      const EdgeInsets.only(bottom: 15, right: 15, left: 15),
+                  padding: const EdgeInsets.all(10),
+                  width: MediaQuery.of(context).size.width,
+                  child: Row(
+                    children: [
+                      SvgPicture.asset(
+                        'assets/pi_index.svg',
+                        width: 24,
+                        height: 24,
+                      ),
+                      const Text('1~50'),
+                      const Spacer(),
+                      const Icon(Icons.keyboard_arrow_down)
+                    ],
+                  ),
+                )
+              ]),
+            ),
+            const SizedBox(height: 15),
+            Padding(
+              padding: _padding,
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  'モード選択',
+                  style: Theme.of(context).textTheme.headline1,
+                ),
+              ),
+            ),
+            TappableCard(child: Text('練習モード'), onTap: null),
+            const SizedBox(height: 15),
+            TappableCard(child: Text('本番モード'), onTap: null),
+          ],
         ),
       ),
     );
---
+++ lib/view/pi_memorization/pi_view.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import '../../model/pi_memorization/pi.dart';
+
+class PiView extends StatelessWidget {
+  final int digitsId;
+  const PiView({Key? key, required this.digitsId}) : super(key: key);
+
+  static final Border _border = Border.all(width: 0.5, color: Colors.grey);
+  static const BorderRadius _borderRadius =
+      BorderRadius.all(Radius.circular(13));
+  static const double _fontSize = 28;
+
+  @override
+  Widget build(BuildContext context) {
+    //IDに応じた桁の円周率を10文字ごとに分けてリストに格納する
+    final Pi pi = Pi.getSubstring(digitsId: digitsId);
+    final RegExp regExp = RegExp(r'(\d{10})(?=(\d{10})+)');
+    final List<String> result = pi.substring
+        .replaceAllMapped(regExp, ((match) => '${match[1]}\n'))
+        .split('\n');
+
+    List<Widget> viewList() {
+      List<Widget> tmpList = [];
+      //一番目のモードであれば整数部分を表示する
+      if (digitsId == 1) {
+        tmpList.add(Align(
+            alignment: Alignment.centerLeft,
+            child: Padding(
+              padding: const EdgeInsets.only(left: 10),
+              child: letterWidget(context, '3.'),
+            )));
+      }
+      //各10文字ごとの列に対して、更に1文字ずつ分割して画面に均等に配置する
+      for (String line in result) {
+        tmpList.add(Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: line
+              .split('')
+              .map((letter) => letterWidget(context, letter))
+              .toList(),
+        ));
+      }
+      return tmpList;
+    }
+
+    return Container(
+      decoration: BoxDecoration(
+        border: _border,
+        borderRadius: _borderRadius,
+      ),
+      margin: const EdgeInsets.all(15),
+      width: MediaQuery.of(context).size.width,
+      child: Column(
+        children: viewList(),
+      ),
+    );
+  }
+
+  Widget letterWidget(BuildContext context, String letter) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 10),
+      width: _fontSize,
+      height: _fontSize,
+      child: Text(
+        letter,
+        style: GoogleFonts.inter(
+            textStyle: const TextStyle(fontSize: _fontSize),
+            fontWeight: FontWeight.w500),
+        textAlign: TextAlign.center,
+      ),
+    );
+  }
+}
