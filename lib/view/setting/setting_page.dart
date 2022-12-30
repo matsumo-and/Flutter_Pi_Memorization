94e83aa89b43f4ab081417e5dd2e02eb40400b44
matsumo-and <yh134.toisanda@gmail.com>
2022-12-31 02:31:04 +0900

    [feat]implement setting page

--- lib/view/setting/setting_page.dart
+++ lib/view/setting/setting_page.dart
@@ -1,9 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_pi_memorization/view/multiplication/tappable_card.dart';
-import 'package:flutter_pi_memorization/view/setting/privacy_policy.dart';
+import 'package:flutter_pi_memorization/view/setting/web_view.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 
-class SettingPage extends StatelessWidget {
+class SettingPage extends StatefulWidget {
   const SettingPage({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => SettingPageState();
+}
+
+class SettingPageState extends State<SettingPage> {
+  static const double _height = 50;
+  static const EdgeInsets _margin = EdgeInsets.fromLTRB(15, 15, 15, 0);
+  static const EdgeInsets _padding = EdgeInsets.all(10);
+
+  String _version = '';
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) => getVersion());
+  }
+
+  Future<void> getVersion() async {
+    PackageInfo packageInfo = await PackageInfo.fromPlatform();
+    setState(() {
+      _version = packageInfo.version;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,21 +40,66 @@ class SettingPage extends StatelessWidget {
           child: Column(
         children: [
           TappableCard(
-              height: 50,
-              margin: const EdgeInsets.all(15),
+              height: _height,
+              margin: _margin,
               child: Padding(
-                padding: const EdgeInsets.all(10),
+                padding: _padding,
                 child: Row(
                   crossAxisAlignment: CrossAxisAlignment.center,
-                  children: const [
-                    Icon(Icons.lock),
-                    SizedBox(width: 10),
-                    Text('プライバシーポリシーを見る'),
+                  children: [
+                    const Icon(Icons.lock_outline),
+                    SizedBox(width: _padding.bottom),
+                    const Text('プライバシーポリシー'),
                   ],
                 ),
               ),
               onTap: () => Navigator.of(context).push(MaterialPageRoute(
-                  builder: (context) => const PrivacyPolicyView()))),
+                  fullscreenDialog: true,
+                  builder: (context) => const SettingWebView(
+                        url: 'https://mathpire.studio.site/privacy-policy',
+                      )))),
+          TappableCard(
+              height: _height,
+              margin: _margin,
+              child: Padding(
+                padding: _padding,
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    const Icon(Icons.mail_outline),
+                    SizedBox(width: _padding.bottom),
+                    const Text('コンタクト'),
+                  ],
+                ),
+              ),
+              onTap: () => Navigator.of(context).push(MaterialPageRoute(
+                  fullscreenDialog: true,
+                  builder: (context) => const SettingWebView(
+                        url: 'https://mathpire.studio.site/contact',
+                      )))),
+          TappableCard(
+              height: _height,
+              margin: _margin,
+              child: Padding(
+                padding: _padding,
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    const Icon(Icons.info_outline),
+                    SizedBox(width: _padding.bottom),
+                    const Text('Mathpireについて'),
+                  ],
+                ),
+              ),
+              onTap: () => Navigator.of(context).push(MaterialPageRoute(
+                  fullscreenDialog: true,
+                  builder: (context) => const SettingWebView(
+                        url: 'https://mathpire.studio.site/',
+                      )))),
+          const SizedBox(height: 15),
+          Text('Mathpire Ver$_version'),
+          const SizedBox(height: 15),
+          const Text('Copyright © 2023 SpiralLab, Inc. All right reserved.'),
         ],
       )),
     );
